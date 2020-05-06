eb7ec6ccdf1b94fe7eb37d5ffcaaf34be185d408
Iftekhar Rahman <iftekharmail1@gmail.com>
2020-05-06 10:04:50 -0700

    sda

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,40 +85,40 @@ jQuery(document).ready(function($){
     //     }
     // });
 
-    // $(".feature-carousel-wrap").owlCarousel({
-    //     items: 4,
-    //     loop: true,
-    //     nav: true,
-    //     dots: true,
-    //     autoplay: false,
-    //     margin: 15,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    //     responsive: {
-    //         0: {
-    //             items: 1,
-    //             margin: 0,
-    //             nav: false
-    //         },
-    //         481: {
-    //             items: 2,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         720: {
-    //             items: 3,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         960: {
-    //             items: 3,
-    //             margin: 25,
-    //         },
-    //         1140: {
-    //             items: 4
-    //         }
+    $(".featured-cars").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: false,
+        margin: 15,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        // responsive: {
+        //     0: {
+        //         items: 1,
+        //         margin: 0,
+        //         nav: false
+        //     },
+        //     481: {
+        //         items: 2,
+        //         margin: 15,
+        //         nav: false
+        //     },
+        //     720: {
+        //         items: 3,
+        //         margin: 15,
+        //         nav: false
+        //     },
+        //     960: {
+        //         items: 3,
+        //         margin: 25,
+        //     },
+        //     1140: {
+        //         items: 4
+        //     }
 
-    //     }
-    // });
+        // }
+    });
 
 
 
